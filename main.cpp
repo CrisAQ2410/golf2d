fc3c1ed449b467d5c963246049bea7a328b4bd51
Chu Anh Quoc <chuanhquoc2410@gmail.com>
2024-03-06 12:37:31 +0700

    quoc

--- main.cpp
+++ main.cpp
@@ -47,7 +47,7 @@ bool loadMedia()
 {
 	bool success = true;
 
-	gHelloWorld = IMG_Load( "D:\\vscode\\game project\\prj\\background.jpg" );
+	gHelloWorld = IMG_Load( "img_src/background.jpg" );
 	if( gHelloWorld == NULL )
 	{
 		printf( "Unable to load image %s! SDL Error: %s\n", "02_getting_an_image_on_the_screen/hello_world.bmp", SDL_GetError() );
